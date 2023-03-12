890b2799e90ef0b868e33a508dde780bd42bf326
kjartanleander <Kjar0321@stud.kea.dk>
2023-03-12 15:09:43 +0100

    Tilføjet countPowerDTO med alt

--- src/main/java/com/example/superherov4/DTO/CountPowerDTO.java
+++ src/main/java/com/example/superherov4/DTO/CountPowerDTO.java
@@ -1,5 +1,39 @@
 package com.example.superherov4.DTO;
 
 public class CountPowerDTO {
+    public void setSuperHeroName(String superHeroName) {
+        this.superHeroName = superHeroName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public void setCountPower(int countPower) {
+        this.countPower = countPower;
+    }
+
+    private String superHeroName;
+    private String realName;
+    private int countPower;
+
+    public String getSuperHeroName() {
+        return superHeroName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public int getCountPower() {
+        return countPower;
+    }
+
+    public CountPowerDTO(String superHeroName, String realName, int countPower) {
+        this.superHeroName = superHeroName;
+        this.realName = realName;
+        this.countPower = countPower;
+    }
+
 
 }
